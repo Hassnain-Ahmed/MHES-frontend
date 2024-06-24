ca9622b2e0d16649db478459eb792594048850a9
Hassnain Ahmed <ahmedhasnain625@gmail.com>
2024-06-25 02:15:28 +0500

    Updated | PatientSessions

--- src/components/PatientSessions.jsx
+++ src/components/PatientSessions.jsx
@@ -2,7 +2,7 @@
 const PatientSessions = () => {
     return (
         <div className="border-2 border-dashed border-neutral-400 rounded-md p-5">
-            Patient Sessions
+            Patient Sessions Will go here
         </div>
     )
 }
